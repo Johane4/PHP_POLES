fa4b17961a18c05e3e6b862a948bb9743d05b625
Johane4 <omiscar_johane@ymail.com>
2025-11-12 12:58:51 +0100

    correction post method

--- EXOS/02_POST-Correct/traitement.php
+++ EXOS/02_POST-Correct/traitement.php
@@ -36,7 +36,7 @@
     /** -----------------------------------------
      *  Exercice 2 : Affichage sécurisé
      * ------------------------------------------
-     * Utilisation de htmlspecialchars() pour éviter les XSS.
+     * Objectif : Afficher les données du formulaire de manière sécurisée pour les éviter les attaques XSS (échapper les données) (possibilité de copier/coller le traitement de l'exercice 1)
      */
     echo '<h2>Exercice 2 : Affichage sécurisé</h2>';
 
@@ -62,7 +62,13 @@
     /** -----------------------------------------
      *  Exercice 3 : Réaffichage des données
      * ------------------------------------------
-     * Les champs du formulaire sont déjà pré-remplis au-dessus.
+     * 
+     *  Objectif : Réafficher les données précédemment saisies dans le formulaire après la soumission
+     * 
+     *  1 . Pré-remplir les champs du formulaire avec les valeurs soumises précédemment 
+     * 
+     *  *  Cet exercice se fera donc dans la partie formulaire directement ! (Vous devrez supprimer la partie action du formulaire pour qu'il redirige la requête sur la même page)
+     * 
      */
     echo '<h2>Exercice 3 : Réafficher les données précédentes</h2>';
     echo "<p>Les champs saisis sont automatiquement réaffichés grâce aux valeurs dans les inputs.</p>";
@@ -74,7 +80,12 @@
     /** -----------------------------------------
      *  Exercice 4 : Limitation de longueur du mot de passe
      * ------------------------------------------
-     * Vérifier que le mot de passe ne dépasse pas 130 caractères.
+     *  *  Objectif : Limiter la longueur de le mot de passe  saisie par l'utilisateur à 130 caractères et afficher un message d'avertissement si ce seuil est dépassé
+     * 
+     *  1 . Verifier la longueur du champ mot de passe 
+     * 
+     *  2 . Afficher un message si la longueur dépasse 130 caractères
+     * 
      */
     echo '<h2>Exercice 4 : Limitation de longueur du mot de passe</h2>';
 
@@ -97,7 +108,11 @@
     /** -----------------------------------------
      *  Exercice 5 : Conversion de données
      * ------------------------------------------
-     * Convertir la ville en majuscules et afficher un message.
+     * Objectif : Convertir la ville en majuscule avant de l'afficher et afficher un message de confirmation 
+     * 
+     *  1 . Convertir la ville reçue du formulaire en majuscule
+     * 
+     *  2 . Afficher la ville en majuscule ainsi qu'un message de confirmation 'merci pour votre soumission'
      */
     echo '<h2>Exercice 5 : Conversion de données</h2>';
 
